81490d637cf4e20225037cb50bbcedfab5654722
David G. Moore, Jr <david@dgmjr.io>
2023-04-02 00:41:01 -0500

    fixin shit

--- Test/AnEnum.cs
+++ Test/AnEnum.cs
@@ -1,6 +1,19 @@
-namespace TestingDgmjrSdk;
+/* 
+ * AnEnum.cs
+ * 
+ *   Created: 2023-03-28-02:15:28
+ *   Modified: 2023-03-28-03:18:18
+ * 
+ *   Author: David G. Moore, Jr. <david@dgmjr.io>
+ *   
+ *   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */ 
 
-[GenerateEnumerationRecordStruct("AnEnumeration", "TestingJwc")]
+namespace TestingDgmjrSdk;
+using System.ComponentModel.DataAnnotations;
+
+[GenerateEnumerationRecordStruct("AnEnumeration", "TestingDgmjrSdk")]
 public enum AnEnum
 {
     /// <summary>This is a summary of the enum value.</summary>
@@ -19,5 +32,6 @@ public enum AnEnum
     /// <value>The enum value's value.</value>
     /// <example>Foo!</example>
     /// <default>Bar!</default>
+    [Display(Name = "Enumeration Value 3")]
     EnumValue3
 }
