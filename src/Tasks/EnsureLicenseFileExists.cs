b9f4d2f7ec4521320fae48dde425112ce36776b6
David G. Moore, Jr. <david@dgmjr.io>
2024-01-24 13:57:30 -0500

    A few style changes

--- src/Tasks/EnsureLicenseFileExists.cs
+++ src/Tasks/EnsureLicenseFileExists.cs
@@ -1,12 +1,16 @@
 namespace Dgmjr.Sdk.Tasks;
+
 using Microsoft.Build.Framework;
 
 public class EnsureLicenseFileExists : MSBTask
 {
     const string LICENSE_MD = "LICENSE.md";
-    private string ProjectDirectory => Path.GetDirectoryName(MSBuildProjectFullPath);
+    private string ProjectDirectory => GetDirectoryName(MSBuildProjectFullPath);
 
-    private string LicenseFilePath => Path.Combine(ProjectDirectory, LICENSE_MD);
+    private string LicenseFilePath => Combine(ProjectDirectory, LICENSE_MD);
+
+    protected virtual Stream LicenseFileContent =>
+        typeof(EnsureLicenseFileExists).Assembly.GetManifestResourceStream(LICENSE_MD);
 
     [Required]
     public string MSBuildProjectFullPath { get; set; }
@@ -15,10 +19,12 @@ public class EnsureLicenseFileExists : MSBTask
     {
         if (!File.Exists(LicenseFilePath))
         {
-            Log.LogWarning($"License file '{LICENSE_MD}' not found in project directory: {ProjectDirectory}; adding it.");
+            Log.LogWarning(
+                $"License file '{LICENSE_MD}' not found in project directory: {ProjectDirectory}; adding it."
+            );
             using var fs = File.Create(LicenseFilePath);
-            using var manifestStream = typeof(EnsureLicenseFileExists).Assembly.GetManifestResourceStream(LICENSE_MD);
-            manifestStream.CopyTo(fs);
+            using var licenseFileStream = LicenseFileContent;
+            licenseFileStream.CopyTo(fs);
         }
 
         return true;
